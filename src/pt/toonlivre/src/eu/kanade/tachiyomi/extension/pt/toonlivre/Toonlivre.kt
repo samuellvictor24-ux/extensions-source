ab1fd5b91e0435443eb65a30128e692468e390c2
samuellvictor24-ux <seu-email@exemplo.com>
2026-07-14 00:00:55 -0300

    Fix build: Ajusta formatação Spotless e sintaxe do OkHttpClient

--- src/pt/toonlivre/src/eu/kanade/tachiyomi/extension/pt/toonlivre/Toonlivre.kt
+++ src/pt/toonlivre/src/eu/kanade/tachiyomi/extension/pt/toonlivre/Toonlivre.kt
@@ -3,30 +3,25 @@ package eu.kanade.tachiyomi.extension.pt.toonlivre
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import keiyoushi.annotation.Source
 import okhttp3.Interceptor
-import okhttp3.Response
+import okhttp3.OkHttpClient
 
 @Source
 abstract class Toonlivre : Madara() {
 
-    // Intercepta e corrige URLs legadas retornadas pela estrutura do site
-    override val client = super.client.newBuilder()
-        .addInterceptor(::domainRedirectInterceptor)
+    override val client: OkHttpClient = super.client.newBuilder()
+        .addInterceptor(
+            Interceptor { chain ->
+                val request = chain.request()
+                val urlString = request.url.toString()
+                if (urlString.contains("mangalivre.net") || urlString.contains("mangalivre.tv")) {
+                    val newUrl = request.url.newBuilder()
+                        .host("toonlivre.net")
+                        .build()
+                    chain.proceed(request.newBuilder().url(newUrl).build())
+                } else {
+                    chain.proceed(request)
+                }
+            }
+        )
         .build()
-
-    private fun domainRedirectInterceptor(chain: Interceptor.Chain): Response {
-        val request = chain.request()
-        val urlString = request.url.toString()
-        
-        // Verifica se a URL extraída contém o domínio morto
-        if (urlString.contains("mangalivre.net") || urlString.contains("mangalivre.tv")) {
-            val newUrl = request.url.newBuilder()
-                .host("toonlivre.net")
-                .build()
-            
-            // Continua a requisição com o domínio host corrigido
-            return chain.proceed(request.newBuilder().url(newUrl).build())
-        }
-        
-        return chain.proceed(request)
-    }
 }
